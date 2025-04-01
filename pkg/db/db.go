09071fe6f57bb31edfaeef9490050bfd641bf293
Rayten225 <vladsosnikov9@gmail.com>
2025-04-01 18:30:20 +0500

    add db link and test

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,62 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"strconv"
+)
+
+type DB struct {
+	pool *pgxpool.Pool
+}
+
+type News struct {
+	id               int
+	name             string
+	description      string
+	publication_date string
+	link             string
+}
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "GoNews"
+)
+
+func New() (*DB, error) {
+	ctx := context.Background()
+	db := DB{}
+	var err error
+	pwd := os.Getenv("dbpass")
+	connStr := "postgres://" + user + ":" + pwd + "@" + host + ":" + strconv.Itoa(port) + "/" + dbname
+	db.pool, err = pgxpool.Connect(ctx, connStr)
+	if err != nil {
+		return nil, err
+	}
+	return &db, nil
+}
+
+func (db *DB) News(col int) [][]string {
+	ctx := context.Background()
+	result := make([][]string, 0)
+	rows, err := db.pool.Query(ctx, "SELECT * FROM news ORDER BY id DESC LIMIT $1;", col)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for rows.Next() {
+		var news News
+		if err := rows.Scan(&news.id, &news.name, &news.description, &news.publication_date, &news.link); err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, []string{strconv.Itoa(news.id), news.name, news.description, news.publication_date, news.link})
+	}
+
+	return result
+}
---
+++ pkg/db/db_test.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4/pgxpool"
+	_ "github.com/lib/pq"
+	"os"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestDB_News(t *testing.T) {
+	ctx := context.Background()
+	db := DB{}
+	var err error
+	pwd := os.Getenv("dbpass")
+	connStr := "postgres://" + user + ":" + pwd + "@" + host + ":" + strconv.Itoa(port) + "/" + dbname
+	db.pool, err = pgxpool.Connect(ctx, connStr)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	type fields struct {
+		pool *pgxpool.Pool
+	}
+	type args struct {
+		col int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   [][]string
+	}{
+		{
+			name: "TestDB_News1",
+			fields: fields{
+				pool: db.pool,
+			},
+			args: args{3},
+			want: [][]string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := db.News(tt.args.col); reflect.DeepEqual(got, tt.want) {
+				t.Errorf("News() = %v, want %v", got, tt.want)
+			} else {
+
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    *DB
+		wantErr bool
+	}{
+		{
+			name: "TestDB_News1",
+			want: &DB{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := New()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
